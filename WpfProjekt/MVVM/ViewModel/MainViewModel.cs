3fb6860f982306d9aed369ba032140df786d23d8
Michal Napiorkowski <mr.crossfade.mnr@gmail.com>
2023-05-07 11:42:35 +0200

    frontend

---
+++ WpfProjekt/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfProjekt.Core;
+
+namespace WpfProjekt.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObj
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DisViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryVModel DisVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                    OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM= new HomeViewModel();
+            DisVM= new DiscoveryVModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DisViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DisVM;
+            });
+        }
+    }
+}
