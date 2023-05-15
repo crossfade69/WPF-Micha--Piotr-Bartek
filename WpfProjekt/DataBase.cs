edd745b115b195182d215504bf4eadf1dc03d083
bartekch1235 <82156135+bartekch1235@users.noreply.github.com>
2023-05-15 18:11:10 +0200

    test copmmit
    test  klas

---
+++ WpfProjekt/DataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class DataBase
+    {
+    public List<User> users = new List<User>();
+    public List<Game> allGames = new List<Game>();
+    public DataBase() 
+    {
+
+    }  
+    public User Login(string log,string pas)
+    {
+        foreach (var user in users) 
+        {
+            if(user.Login==log&&user.password==pas)
+                return user;
+        }
+
+        return null;
+    }
+    public List<Game> GetUserGames() 
+    {
+
+    }
+    
+
+    }
+
